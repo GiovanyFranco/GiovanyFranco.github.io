72a48916a9f17492d3e077b6bb03d2eeeee8ed1b
CesarF <cfranco1901@gmail.com>
2025-03-06 12:33:39 -0500

    Solucionar error 404 al refrescar en GitHub Pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 py-8">
       <div className="container mx-auto px-4 text-center">
-        <p className="text-gray-400">&copy; {new Date().getFullYear()} Giovany Franco - Ingeniero Industrial. Todos los derechos reservados.</p>
+        <p className="text-gray-400">&copy; {new Date().getFullYear()} Giovany Franco - Todos los derechos reservados.</p>
       </div>
     </footer>
   );
